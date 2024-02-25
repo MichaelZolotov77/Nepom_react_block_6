8caaf400f49ea3eebdf1b1b4f93158796b11c00d
MichaelZolotov77 <michaelzol@yandex.ru>
2024-02-25 21:53:32 +0300

    Два стейта

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 
 function App() {
   const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(false)
 
   const increment = () => {
     setCount(count + 1)
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <button onClick={increment}>{count}</button>
+      <button onClick={() => setLoading(true)}>{loading.toString()}</button>
     </div>
   )
 }
