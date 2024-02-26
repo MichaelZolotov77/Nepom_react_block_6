a38162490f43e0d7be9fec2d2e677fa941261418
MichaelZolotov77 <michaelzol@yandex.ru>
2024-02-26 19:37:01 +0300

    useRef не приводит к перерендеру

--- src/Ref.jsx
+++ src/Ref.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useRef } from "react"
 
 const WithRef = () => {
-  const inputEl = useRef(null)
-  console.log(inputEl)
+  const numRef = useRef(0)
 
-  useEffect(() => console.log(inputEl))
+  const handleClick = () => {
+    numRef.current = 1
+  }
+
+  useEffect(() => console.log(numRef.current))
   return (
     <div>
       <h1>React app</h1>
-      <input placeholder="name" ref={inputEl} />
+      <button onClick={handleClick}>{numRef.current}</button>
     </div>
   )
 }
