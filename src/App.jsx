1c873ecf5892dfd20c707077b6a2095d2778003f
MichaelZolotov77 <michaelzol@yandex.ru>
2024-02-26 20:34:56 +0300

    Рефакторинг таймера через хуки, начальный код

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react"
-import Clicker from "./Clicker"
-import WithRef from "./Ref"
+import Timer from "./Timer"
 
 function App() {
-  const [isClicker, setClicker] = useState(false)
+  const [isTimer, setTimer] = useState(false)
 
   return (
     <div className="App">
-      <WithRef />
+      <h2>React App</h2>
+      <button onClick={() => setTimer(!isTimer)}>Toggle timer</button>
+      {isTimer && <Timer />}
     </div>
   )
 }
---
+++ src/Timer.jsx
@@ -0,0 +1,55 @@
+import React from "react"
+
+export default class Timer extends React.Component {
+  state = {
+    count: 0,
+    isCounting: false,
+  }
+
+  handleReset = () => {
+    this.setState({
+      count: 0,
+      isCounting: false,
+    })
+  }
+
+  handleStart = () => {
+    this.setState({
+      isCounting: true,
+    })
+    this.counterId = setInterval(() => {
+      this.setState({ count: this.state.count + 1 })
+    }, 1000)
+  }
+
+  handleStop = () => {
+    clearInterval(this.counterId)
+    this.setState({
+      isCounting: false,
+    })
+  }
+
+  componentDidMount() {
+    const userCount = localStorage.getItem("count")
+    if (userCount) this.setState({ count: +userCount })
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem("count", this.state.count)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.counterId)
+    this.handleStop()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h3>{this.state.count}</h3>
+        {!this.state.isCounting ? <button onClick={this.handleStart}>Start</button> : <button onClick={this.handleStop}>Stop</button>}
+        <button onClick={this.handleReset}>Reset</button>
+      </div>
+    )
+  }
+}
